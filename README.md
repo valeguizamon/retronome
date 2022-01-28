31274ba27e38d4e9c9952698799d28144c05f9f6
Valentín Leguizamón Sevilla <valentin.leguizamon.sevilla@gmail.com>
2022-01-28 00:50:18 -0300

    Commit Inicial - Creacion y ordenado en carpetas del proyecto

---
+++ README.md
@@ -0,0 +1,4 @@
+# retronome - React.js
+
+Proyecto para el curso de React.js de CoderHouse
+
---
+++ src/App.js
@@ -0,0 +1,25 @@
+import logo from './assets/images/logo.svg';
+import './assets/css/App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Proyecto E-Comerce recién creado.
+        </p>
+        <a
+          className="App-link"
+          href="https://github.com/valeguizamon"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Valentín Leguizamón Sevilla
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
