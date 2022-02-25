22a83ab0639c9db392333c08dd3efabe58cab833
Valentín Leguizamón Sevilla <valentin.leguizamon.sevilla@gmail.com>
2022-02-24 23:03:56 -0300

    Cambios en la navegacion, se utilizan Links

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import NavBar from "./components/NavBar";
 
-import Home from "./pages/Home";
 import About from "./pages/About";
-import ItemDetailContainer from "./components/ItemDetailContainer";
 import Error404 from "./pages/Error404";
+import ItemListContainer from "./components/ItemListContainer"
+import ItemDetailContainer from "./components/ItemDetailContainer"
 
 const App = () => {
 
@@ -12,10 +12,10 @@ const App = () => {
     <Router>
       <NavBar />
       <Routes>
-        <Route exact path="/" element={<Home />} />
-        <Route path="/home" element={<Home />} />
+        <Route exact path="/" element={<ItemListContainer />} />
+        <Route path="/home" element={<ItemListContainer />} />
         <Route path="/detail/:id" element={<ItemDetailContainer />} />
-        <Route path="/category/:idCategory" element={<Home />} />
+        <Route path="/category/:idCategory" element={<ItemListContainer />} />
         <Route path="/about" element={<About />} />
         <Route path="*" element={<Error404 />} />
       </Routes>
--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,31 +1,33 @@
+import { Link } from "react-router-dom";
 import CartWidget from "./CartWidget";
 
+
 const NavBar = () => {
-    
+
     return (
         <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
             <div className="container-fluid">
                 <ul className="navbar-nav float-right">
-                    <a className="navbar-brand float-center" href="/home">
+                    <Link className="navbar-brand float-center" to="/home">
                         <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-joystick me-2" viewBox="0 0 16 16">
                             <path d="M10 2a2 2 0 0 1-1.5 1.937v5.087c.863.083 1.5.377 1.5.726 0 .414-.895.75-2 .75s-2-.336-2-.75c0-.35.637-.643 1.5-.726V3.937A2 2 0 1 1 10 2z" />
                             <path d="M0 9.665v1.717a1 1 0 0 0 .553.894l6.553 3.277a2 2 0 0 0 1.788 0l6.553-3.277a1 1 0 0 0 .553-.894V9.665c0-.1-.06-.19-.152-.23L9.5 6.715v.993l5.227 2.178a.125.125 0 0 1 .001.23l-5.94 2.546a2 2 0 0 1-1.576 0l-5.94-2.546a.125.125 0 0 1 .001-.23L6.5 7.708l-.013-.988L.152 9.435a.25.25 0 0 0-.152.23z" />
                         </svg>
                         retronome
-                    </a>
+                    </Link>
                     <li className="nav-item">
-                        <a className="nav-link" href="/">Home</a>
+                        <Link className="nav-link" to="/">Home</Link>
                     </li>
                     <li className="nav-item">
-                        <a className="nav-link" href="/about">About</a>
+                        <Link className="nav-link" to="/about">About</Link>
                     </li>
                     <li className="nav-item dropdown">
                         <a className="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
                             Categories
                         </a>
                         <ul className="dropdown-menu" aria-labelledby="navbarDropdown">
-                            <li><a className="dropdown-item" href="/category/1">Games</a></li>
-                            <li><a className="dropdown-item" href="/category/2">Consoles</a></li>
+                            <li><Link className="dropdown-item" to="/category/1">Games</Link></li>
+                            <li><Link className="dropdown-item" to="/category/2">Consoles</Link></li>
                         </ul>
                     </li>
                 </ul>
@@ -58,5 +60,5 @@ const NavBar = () => {
     );
 
 }
- 
+
 export default NavBar;
